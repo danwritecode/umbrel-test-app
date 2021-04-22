0c28ab65615b9fa29cf977078ad3aed809fb5426
nelsynelz <dnelson4993@outlook.com>
2021-04-22 14:19:00 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Import tailwind styles
+import './index.css'
+
 createApp(App).use(store).use(router).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        rose: colors.rose,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+  ],
+}
